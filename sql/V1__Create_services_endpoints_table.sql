064f031995d6ff918a60cb8e638ba74c8c1b332b
Tristan Sloughter <t@crashfast.com>
2019-07-03 08:33:19 -0600

    i think the tables are in their final state now

--- sql/V1__Create_services_endpoints_table.sql
+++ sql/V1__Create_services_endpoints_table.sql
@@ -1,28 +1,29 @@
 CREATE EXTENSION IF NOT EXISTS hstore;
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE named_ports
-    (
-        service_id UUID NOT NULL,
-        protocol TEXT,
-        port_name TEXT,
-        port INT2,
-        PRIMARY KEY (service_id, port_name)
-    );
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE services
     (
         id UUID NOT NULL DEFAULT gen_random_uuid(),
-        name TEXT NOT NULL,
+        name TEXT UNIQUE NOT NULL,
         attributes HSTORE NOT NULL,
         created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
         updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-        PRIMARY KEY (name)
+        PRIMARY KEY (id)
     );
+
+CREATE TABLE named_ports
+    (
+        service_id UUID NOT NULL REFERENCES services (id) ON DELETE CASCADE,
+        port_name TEXT,
+        protocol TEXT,
+        port INT2,
+        PRIMARY KEY (service_id, port_name)
+    );
+
 
 CREATE TABLE endpoints
     (
-        service_id UUID NOT NULL,
+        service_id UUID NOT NULL REFERENCES services (id) ON DELETE CASCADE,
         ip INET NOT NULL,
         tags TEXT[] NOT NULL,
         created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
