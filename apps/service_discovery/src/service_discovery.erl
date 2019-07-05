f91727fd07706eb0ba8732c393fdb0cb3efcb04b
Tristan Sloughter <t@crashfast.com>
2019-07-05 09:13:06 -0600

    add specs to public interface

--- apps/service_discovery/src/service_discovery.erl
+++ apps/service_discovery/src/service_discovery.erl
@@ -28,20 +28,26 @@
               tag/0,
               attributes/0]).
 
+-spec create(service()) -> binary() | {error, term()}.
 create(Service) ->
     sdp_services_storage:create(Service).
 
+-spec lookup(unicode:unicode_binary()) -> service() | {error, term()}.
 lookup(ServiceName) ->
     sdp_services_storage:read(ServiceName).
 
+-spec lookup_endpoints(unicode:unicode_binary()) -> [endpoint()] | {error, term()}.
 lookup_endpoints(ServiceName) ->
     sdp_services_storage:read_endpoints(ServiceName).
 
+-spec add_named_ports(unicode:unicode_binary(), named_ports()) -> ok | {error, term()}.
 add_named_ports(ServiceName, NamedPorts) ->
     sdp_services_storage:add_named_ports(ServiceName, NamedPorts).
 
+-spec list() -> [service()] | {error, term()}.
 list() ->
     sdp_services_storage:list().
 
+-spec register(name(), endpoint()) -> ok.
 register(ServiceName, Endpoint) ->
     sdp_services_storage:register(ServiceName, Endpoint).
