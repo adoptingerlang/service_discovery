d96fa77684c277732590eb6e4c579c884a88b39d
Tristan Sloughter <t@crashfast.com>
2023-09-25 10:47:30 -0600

    remove unnused healthcheck and shutting down term
    healthz is no longer needed because the liveness check is removed.
    
    Toggling to shutting down and returning a 503 from the ready check
    isn't useful because the pod will already be being removed from the
    routes and readiness checks are infrequent -- mainly used for
    autoscaling.

--- apps/service_discovery_http/src/service_discovery_http_app.erl
+++ apps/service_discovery_http/src/service_discovery_http_app.erl
@@ -3,23 +3,12 @@
 -behavior(application).
 
 -export([start/2,
-         prep_stop/1,
          stop/1]).
-
--export([is_shutting_down/0]).
-
--define(IS_SHUTTING_DOWN, {?MODULE, is_shutting_down}).
 
 start(_, _) ->
     Env = application:get_all_env(service_discovery_http),
     service_discovery_http_sup:start_link(Env).
 
-prep_stop(State) ->
-    persistent_term:put(?IS_SHUTTING_DOWN, true),
-    State.
-
 stop(_) ->
     ok.
 
-is_shutting_down() ->
-    persistent_term:get(?IS_SHUTTING_DOWN, false).
